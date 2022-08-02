b8e0aeaa5951eb7f41ff10000b56ab18a2233731
Antonio Addis <antonio.addis@inaf.it>
2022-08-02 18:12:31 +0200

    bugfix in dictionary

--- comet/plugins/eventreceiver.py
+++ comet/plugins/eventreceiver.py
@@ -19,7 +19,9 @@ from astropy.coordinates import SkyCoord
 
 from comet.plugins.Voevent import Voevent
 
-db_config = {user: 'afiss', password: '', host: '127.0.0.1', port: 60306, database: 'afiss_rta_pipe_test_3'}
+
+# Server parameters
+db_config = {'user': 'afiss', 'password': '', 'host': '127.0.0.1', 'port': 60306, 'database': 'afiss_rta_pipe_test_3'}
 
 # Event handlers must implement IPlugin and IHandler.
 @implementer(IPlugin, IHandler)
@@ -34,8 +36,8 @@ class EventReceiver(object):
         try:
 
             cnx = mysql.connector.connect(user=db_config['user'], password=os.environ["MYSQL_AFISS"],
-                              host=['host'], port=['port'],
-                              database=['database'])
+                              host=db_config['host'], port=db_config['port'],
+                              database=db_config['database'])
 
         except mysql.connector.Error as err:
             if err.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
